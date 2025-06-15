b8246efb9787bc8a92a87c407207fa451f567ab7
Murat <akdemir_murat@outlook.com>
2025-06-16 00:16:02 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -14,36 +14,4 @@ builder.Services.AddOpenApi();
 var app = builder.Build();
 app.MapOpenApi();
 app.MapScalarApiReference();
-
-app.MapGet("/", () => "Hello World!");
-
-var crudGroup = app.MapGroup("/Crud").WithOpenApi();
-
-    
-    
-crudGroup.MapPost("/Create/User", (AppDbContext db, User newUser) =>
-{
-    if (!ValidationHelper.ValidateModel(newUser, out var validationResults))
-    {
-        return Results.BadRequest(validationResults);
-    }
-
-    db.Users.Add(newUser);
-    db.SaveChanges();
-    return Results.Ok(newUser);
-});
-crudGroup.MapPost("/Create/Classroom", (AppDbContext db, Classroom newClassroom) =>
-{
-    if (!ValidationHelper.ValidateModel(newClassroom, out var validationResults))
-    {
-        return Results.BadRequest(validationResults);
-    }
-    
-    db.Classrooms.Add(newClassroom);
-    db.SaveChanges();
-    return Results.Ok(newClassroom);
-});
-
-
-
 app.Run();
